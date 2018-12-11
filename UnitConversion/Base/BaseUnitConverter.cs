53ea8c7694d04c562dc350e9b51a8d6836343d11
Simon Bosanquet <40687840+sboptimal@users.noreply.github.com>
2018-12-11 10:58:05 +0000

    Corrected Instantiate to use property for the right unit rather than the backing field

--- UnitConversion/Base/BaseUnitConverter.cs
+++ UnitConversion/Base/BaseUnitConverter.cs
@@ -24,7 +24,7 @@ namespace UnitConversion.Base
             Units = conversionFactors;
 
             UnitLeft = Units.BaseUnit;
-            unitRight = units.BaseUnit;
+            UnitRight = Units.BaseUnit;
         }
 
         /// <summary>
